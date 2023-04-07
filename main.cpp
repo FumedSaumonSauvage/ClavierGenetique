3783c482ae0624915df4effdd77cc7853ef2c578
Simon <simon.hergott@inria.fr>
2023-04-08 00:53:50 +0200

    début du dev (plus de TODO que de code)

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Essentiellement, il n'y a rien.";
+    return 0;
+}
+
+
+//TODO: implémenter les états d'un clavier (créer une classe clavier?)
+//TODO: définir une norme sur le clavier puis sa distance associée (voir pour un truc qui marche en diagonale, genre distance euclidienne)
+//TODO: faire un affichage à peu pres clair
+//TODO: revoir la base de l'algo génétique pur éviter de se planter dans les probabilités de mutation, et revoir comment on fait du crossing over
